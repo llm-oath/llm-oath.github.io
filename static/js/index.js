7bccc256f198d51cd578e35eef2581f6ea17cd26
NancyLi02 <nanli02@outlook.com>
2026-02-20 15:09:38 -0500

    1.0

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,1 @@
 window.HELP_IMPROVE_VIDEOJS = false;
-
-$(document).ready(function() {
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
-    }
-
-    bulmaCarousel.attach('.carousel', options);
-    bulmaSlider.attach();
-})
